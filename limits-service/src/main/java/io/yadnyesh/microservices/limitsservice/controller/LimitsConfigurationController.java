7dda0515db9c3ca7eb463d7eef107fc4360d0501
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-10-22 11:09:43 +0530

    Configuration from Application properties

--- limits-service/src/main/java/io/yadnyesh/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/io/yadnyesh/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -1,16 +1,21 @@
 package io.yadnyesh.microservices.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.yadnyesh.microservices.limitsservice.bean.LimitConfiguration;
+import io.yadnyesh.microservices.limitsservice.config.Configuration;
 
 @RestController
 public class LimitsConfigurationController {
 	
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsConfiguration() {
-		return new LimitConfiguration(1000, 1);
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 	
 }
