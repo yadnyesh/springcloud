3485e073d36b46a069b02e32edfa6e4ff5b2de9b
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-10-22 18:33:02 +0530

    Refactoring code

--- currency-exchange-service/src/main/java/io/yadnyesh/microservices/currencyexchangeservice/model/ExchangeValue.java
+++ currency-exchange-service/src/main/java/io/yadnyesh/microservices/currencyexchangeservice/model/ExchangeValue.java
@@ -1,14 +1,34 @@
-package io.yadnyesh.microservices.currencyexchangeservice.service;
+package io.yadnyesh.microservices.currencyexchangeservice.model;
 
 import java.math.BigDecimal;
 
-public class ExchangeValue {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+
+@Entity
+public class ExchangeValue {
+	
+	@Id
 	private Long id;
+	
+	@Column(name="currency_from")
 	private String from;
+	
+	@Column(name="currency_to")
 	private String to;
 	private BigDecimal conversionMultiple;
+	private int port;
 	
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
 	public ExchangeValue() {
 		
 	}
