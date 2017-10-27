a041e19425c1e24364ce028769aa2b9e32dea2b5
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-10-27 09:55:15 +0530

    Currency Convertor

--- currency-conversion-service/src/main/java/io/yadnyesh/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/io/yadnyesh/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,5 +1,7 @@
 package io.yadnyesh.microservices.currencyconversionservice.controller;
 
+import java.math.BigDecimal;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,9 @@ import io.yadnyesh.microservices.currencyconversionservice.model.CurrencyConvers
 public class CurrencyConversionController {
 	
 	@GetMapping("/currency-convertor/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable String quantity) {
+	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 		
+		return (new CurrencyConversionBean(1L, from, to, BigDecimal.ONE, quantity, quantity, 0));
 		
 	}
 
