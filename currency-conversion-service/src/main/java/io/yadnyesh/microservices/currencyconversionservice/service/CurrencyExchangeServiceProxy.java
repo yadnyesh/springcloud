b29eaa14a1a11c0cb78a78315c986f99c300d6bc
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-11-14 11:19:37 +0530

    Changes to enable Ribbon

--- currency-conversion-service/src/main/java/io/yadnyesh/microservices/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/io/yadnyesh/microservices/currencyconversionservice/service/CurrencyExchangeServiceProxy.java
@@ -1,12 +1,15 @@
 package io.yadnyesh.microservices.currencyconversionservice.service;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import io.yadnyesh.microservices.currencyconversionservice.model.CurrencyConversionBean;
 
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
+//@FeignClient(name="currency-exchange-service", url="localhost:8000")
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, 
