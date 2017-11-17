232fc8bcc76b8228078dc2932cc67ddd8e784f1b
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-11-17 12:27:27 +0530

    Adding Filter

--- netflix-zuul-api-gateway/src/main/java/io/yadnyesh/microservices/netflixzuulapigateway/ZuulLoggingFilter.java
+++ netflix-zuul-api-gateway/src/main/java/io/yadnyesh/microservices/netflixzuulapigateway/ZuulLoggingFilter.java
@@ -4,10 +4,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 
+@Component
 public class ZuulLoggingFilter extends ZuulFilter{
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
